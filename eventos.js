74b356ca78d756ea9a8cd250101de9560f72627b
camilapessina <capessinab@gmail.com>
2024-08-12 19:16:26 -0300

    cambiosss

--- eventos.js
+++ eventos.js
@@ -9,7 +9,7 @@ function ClickEnDiv() {
 
 function clickEnBoton(evento) {
     evento.stopPropagation(); 
-    alert('Hola!');
+    alert('Hola!!');
 }
 
 miDiv.addEventListener('click', ClickEnDiv);
